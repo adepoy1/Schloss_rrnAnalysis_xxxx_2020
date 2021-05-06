bd87d7cd7d7d34a5f1a07d27120ca4c9317ed6f4
Amber DePoy <adepoy12@gmail.com>
2021-05-06 13:27:01 -0500

    Automate pipeline with Makefile, closes #10

--- code/get_silva_seed.sh
+++ code/get_silva_seed.sh
@@ -5,9 +5,9 @@
 # outputs: places SILVA SEED reference alignment into data/references/silva_seed
 #
 # Download this version of the SILVA reference to help with aligning our sequence data.
-# This is version 138, which was released in 2019. Because the tgz file contains a README file, 
+# This is version 138, which was released in 2019. Because the tgz file contains a README file,
 # we extracted to a directory with data/references
 
 wget -nc -P data/references/  https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138.tgz
 mkdir data/references/silva_seed
-tar xvzf data/references/silva.seed_v138.tgz -C data/references/silva_seed/
+tar xvzmf data/references/silva.seed_v138.tgz -C data/references/silva_seed/
